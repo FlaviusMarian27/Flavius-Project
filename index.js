89e7e78d6686db9c5f77ae9737afa3887d02bf5a
FlaviusMarian27 <marianflavius27@yahoo.com>
2026-02-16 20:28:58 +0200

    Skills part add

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Happy developing ✨')
+//Pentru copyright
+document.getElementById('year').textContent = new Date().getFullYear();
+
+//==============================================================
+// Pentru cartonasele de skilluri
+const scrollers = document.querySelectorAll('.scroller');
+
+if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
+
+function addAnimation() {
+    scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true);
+        const scrollerInner = Array.from(scroller.children);
+        scrollerInner.forEach((item) => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", "true");
+            scroller.appendChild(duplicatedItem);
+        })
+    })
+}
